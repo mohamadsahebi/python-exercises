7f1b91310edbd3ea06f491b2e2aeae641cd265d7
msahebi <m_sahebi@live.com>
2019-05-19 23:07:55 +0430

    add tamrin1

---
+++ tamrin1.py
@@ -0,0 +1,10 @@
+str_list =  ('mohamad  sahebi  sahebi hello is mohamad that is hello')
+lst_list = str_list.split()
+print(lst_list)
+for i in lst_list:
+    count = lst_list.count(i)
+    print(count,i)
+
+    
+
+    
