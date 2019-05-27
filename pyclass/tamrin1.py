06de3bec47eb70321020c4007bb518685fd9aee2
msahebi <m_sahebi@live.com>
2019-05-27 15:58:28 +0430

    add new

--- pyclass/tamrin1.py
+++ pyclass/tamrin1.py
@@ -1,11 +1,11 @@
 str1 = 'mohamad sahebi sahebi is mohamad is is that sahebi that hamid hamed hasan hasan ali ali ali a a a a b b b b w w w w w'
-ls1 = str1.split(" ")
+ls1 = str1.split(' ')
 dic = dict()
 for st1 in  ls1:
     if st1 in dic:
-        dic[st1] =+
+        dic[st1] =+ 1
     else:
-        dic[st1] =1
+        dic[st1] = 1
 print(dic)  
         
 
