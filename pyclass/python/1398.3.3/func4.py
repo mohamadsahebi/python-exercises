66eb63078bd85a157cd6460be9aaa4a58cab33b2
msahebi <msahebi@mitdeb.mitdeb>
2019-05-28 17:21:09 +0430

    add new file

--- pyclass/python/1398.3.3/func4.py
+++ pyclass/python/1398.3.3/func4.py
@@ -1,4 +1,5 @@
 countries = ['Spain 300', 'China 2250', 'usa 1600', 'germany 1500', 'japan 700', 'qatar 55']
 def export(item):
     return(int(item.split(' ')[1]))
-print(sorted(countries, key=export))    
+print(sorted(countries, key=export))   
+
