6f5694d2daf998631deefa907c7ca3621ce04d93
msahebi <msahebi@mitdeb.mitdeb>
2019-06-10 12:33:01 +0430

    add cert doc

--- pyclass/python/1398.3.5/tamrin2.py
+++ pyclass/python/1398.3.5/tamrin2.py
@@ -2,7 +2,7 @@ import os, time, datetime
 folder = input('Enter PWD address : ')
 date_time = os.listdir(folder)
 dic = dict()
-for item in  time.ctime (date_time):
+for item in  date_time:
     if item in dic:
         dic[item] =+ 1
     else:
